76b6d1945aee6c24636380fd266cd4e0851836d4
Naveed Zaheer <nzaheer@microsoft.com>
2020-11-11 15:24:46 -0600

    HTML

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.writeHead(200, {"Content-Type": "text/html"});
     response.end("<H1>Hello World! -1</H1>");
 });
 
